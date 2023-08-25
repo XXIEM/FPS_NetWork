d0e868892a65b755dc07232d67c3f0b0881e7eda
xcaeqwe <879503632@qq.com>
2023-08-25 14:35:31 +0800

    完成管道生成器

--- Source/FPS_Network/FPS_Network.Build.cs
+++ Source/FPS_Network/FPS_Network.Build.cs
@@ -6,6 +6,7 @@ public class FPS_Network : ModuleRules
 {
 	public FPS_Network(ReadOnlyTargetRules Target) : base(Target)
 	{
+		PrivateDependencyModuleNames.AddRange(new string[] { "GeometryScriptingEditor" });
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NetCore","GameplayAbilities","GameplayTags","GameplayTasks","HeadMountedDisplay", "EnhancedInput" });
