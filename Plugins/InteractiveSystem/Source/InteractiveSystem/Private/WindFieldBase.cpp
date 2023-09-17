a2fcfbb17647d4ad1f1e4f98e6a2cac0f9c78fed
xcaeqwe <879503632@qq.com>
2023-09-17 21:30:06 +0800

    1231

---
+++ Plugins/InteractiveSystem/Source/InteractiveSystem/Private/WindFieldBase.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WindFieldBase.h"
+
+
+// Sets default values
+AWindFieldBase::AWindFieldBase()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AWindFieldBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWindFieldBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AddWindField(MeshAttached,WindFieldGenerationInfo);
+}
+
+
+
+
+
+
+
---
+++ Plugins/InteractiveSystem/Source/InteractiveSystem/Private/WindFieldComponent.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WindFieldComponent.h"
+
+#include "ShaderParameterStruct.h"
+#include "InteractiveSystem/Public/WindFieldBase.h"
+
+
+// Sets default values for this component's properties
+UWindFieldComponent::UWindFieldComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWindFieldComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	GenerateWindField_Implementation(FieldGenerationInfos,TargetMesh);
+	// ...
+	
+}
+
+void UWindFieldComponent::GenerateWindField_Implementation(TArray<FWindFieldGenerationInfo>& InFieldGenerationInfos,
+	UPrimitiveComponent* InTargetMesh)
+{
+	const auto World = GetWorld();
+	check(World);
+	for (const FWindFieldGenerationInfo Info : InFieldGenerationInfos)
+	{
+		const auto SpawnedActor  = Cast<AWindFieldBase>(World->SpawnActor(Info.SpawnFieldClass.GetDefaultObject()->GetClass()));
+		if (SpawnedActor == nullptr)
+		{
+			return;
+		}
+		SpawnedActor->AttachToComponent(InTargetMesh,FAttachmentTransformRules::SnapToTargetIncludingScale,Info.SocketName);
+		SpawnedActor->MeshAttached = TargetMesh;
+		SpawnedActor->WindFieldGenerationInfo = Info;
+	}
+}
+
+class FTestShader : public FGlobalShader
+{
+	DECLARE_GLOBAL_SHADER(FTestShader);
+	SHADER_USE_PARAMETER_STRUCT(FTestShader,FGlobalShader);
+	
+	class FMyShaderPerm : SHADER_PERMUTATION_INT("TEST",1);
+	BEGIN_SHADER_PARAMETER_STRUCT(FParameters,)
+	END_SHADER_PARAMETER_STRUCT()
+};
